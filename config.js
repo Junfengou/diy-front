95ed1ce54bd825838ba255262c2cacb96349afca
Junfeng ou <junfengou@gmail.com>
2021-05-09 16:43:16 -0500

    vercel round 2?

--- config.js
+++ config.js
@@ -2,30 +2,3 @@
 export const endpoint = `http://localhost:3000/api/graphql`;
 export const prodEndpoint = `https://api.storageback.space/api/graphql`;
 export const perPage = 4;
-
-// module.exports = {
-// 	async headers() {
-// 		return [
-// 			{
-// 				// matching all API routes
-// 				source: "https://api.storageback.space/api/graphql",
-// 				headers: [
-// 					{ key: "Access-Control-Allow-Credentials", value: "true" },
-// 					{
-// 						key: "Access-Control-Allow-Origin",
-// 						value: "https://api.storageback.space/api/graphql",
-// 					},
-// 					{
-// 						key: "Access-Control-Allow-Methods",
-// 						value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-// 					},
-// 					{
-// 						key: "Access-Control-Allow-Headers",
-// 						value:
-// 							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-// 					},
-// 				],
-// 			},
-// 		];
-// 	},
-// };
