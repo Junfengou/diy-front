d2e04b177db28bcf1d0aa2a5b5cda89fa0cea219
Junfeng ou <junfengou@gmail.com>
2021-05-09 19:05:25 -0500

    vercel

--- config.js
+++ config.js
@@ -4,3 +4,27 @@ export const prodEndpoint = `https://api.storageback.space/api/graphql`;
 export const perPage = 4;
 
 // vercel
+module.exports = {
+	async headers() {
+		return [
+			{
+				// matching all API routes
+				// source: "/api/:path*",
+				baseURL: "https://api.storageback.space/api/graphql",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value:
+							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+					},
+				],
+			},
+		];
+	},
+};
